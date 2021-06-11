8fc8a2b60c80092fa7c99bee82c75b20f664ce58
JonathanIntrica <jonathan@intrica.net>
2021-06-11 14:24:28 +0100

    issue with app module TS error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { ContactComponent } from './contact/contact.component';
 import { RouterModule } from "@angular/router";
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {GoogleMapsModule} from "@angular/google-maps";
+import { BronteWalkComponent } from './the-walks/bronte-walk/bronte-walk.component';
+import { AnneWalkComponent } from './the-walks/anne-walk/anne-walk.component';
+import { CharlotteWalkComponent } from './the-walks/charlotte-walk/charlotte-walk.component';
+import { EmilyWalkComponent } from './the-walks/emily-walk/emily-walk.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +29,11 @@ import {GoogleMapsModule} from "@angular/google-maps";
     PlanYourVisitComponent,
     AboutComponent,
     ContactComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    BronteWalkComponent,
+    AnneWalkComponent,
+    CharlotteWalkComponent,
+    EmilyWalkComponent
   ],
   imports: [
     BrowserModule,
@@ -39,6 +47,10 @@ import {GoogleMapsModule} from "@angular/google-maps";
       { path: 'about', component: AboutComponent },
       { path: 'contact', component: ContactComponent },
       { path: '404', component: PageNotFoundComponent },
+      { path: 'the-walks/anne-walk', component: AnneWalkComponent },
+      { path: 'the-walks/bronte-walk', component: BronteWalkComponent },
+      { path: 'the-walks/charlotte-walk', component: CharlotteWalkComponent },
+      { path: 'the-walks/anne-walk', component: AnneWalkComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       {path: '**', redirectTo: '404', pathMatch: 'full'}
     ])
